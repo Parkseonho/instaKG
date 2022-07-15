bec4b9fa446e782693d2fd753ac77b6a84611c23
parkseonho <tjsgh2103@gmail.com>
2022-07-15 11:07:54 +0900

    답변 등록 및 DB에 반영

--- sbbb/src/main/java/com/mysite/sbbb/answer/controller/AnswerController.java
+++ sbbb/src/main/java/com/mysite/sbbb/answer/controller/AnswerController.java
@@ -1,25 +1,28 @@
 package com.mysite.sbbb.answer.controller;
 
-import com.mysite.sbbb.answer.dao.AnswerRepository;
-import com.mysite.sbbb.answer.domain.Answer;
-import org.springframework.beans.factory.annotation.Autowired;
+import com.mysite.sbbb.answer.AnswerService;
+import com.mysite.sbbb.question.domain.Question;
+import com.mysite.sbbb.question.service.QuestionService;
+import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.ResponseBody;
-
-import java.util.List;
+import org.springframework.web.bind.annotation.RequestParam;
 
 
-@Controller
 @RequestMapping("/answer")
+@RequiredArgsConstructor
+@Controller
 public class AnswerController {
-    @Autowired
-    private AnswerRepository answerRepository;
+    private final QuestionService questionService;
+    private final AnswerService answerService;
 
-    @RequestMapping("/list")
-    @ResponseBody
-    public List<Answer> showAnswerList() {
-        return answerRepository.findAll();
+    @PostMapping("/create/{id}")
+    public String createAnswer(Model model, @PathVariable("id") Integer id, @RequestParam String content){
+        Question question = this.questionService.getQuestion(id);
+        this.answerService.create(question, content);
+        return String.format("redirect:/question/detail/%s", id);
     }
-
 }
