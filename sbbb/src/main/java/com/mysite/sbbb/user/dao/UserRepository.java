f9c76a298542521ab33d1cd8de186447c1730826
Parksenoho <tjsgh2103@gmail.com>
2022-08-10 13:12:42 +0900

    ?

--- sbbb/src/main/java/com/mysite/sbbb/user/dao/UserRepository.java
+++
@@ -1,8 +0,0 @@
-package com.mysite.sbbb.user.dao;
-
-import com.mysite.sbbb.user.domain.User;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface UserRepository extends JpaRepository<User, Long> {
-
-}
